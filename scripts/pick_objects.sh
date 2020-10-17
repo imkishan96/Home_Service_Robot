49abf13595c9383993d44b996e1fe5bb69399943
Kishan Patel <imkishan96@gmail.com>
2020-10-17 08:25:35 -0500

    added pick_objects submodule and script file

---
+++ scripts/pick_objects.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# change the catkin_ws path if you have different path
+source_catkin="source ~/catkin_ws/devel/setup.bash;"
+#change the model if you wanna use another model
+export_model="export TURTLEBOT3_MODEL=burger;"
+
+script_dir=$(dirname $(realpath $0))
+HSR_dir=$(dirname $script_dir)
+launch_dir=$HSR_dir/launch
+
+xterm -e  "
+$source_catkin 
+$export_model
+roslaunch home_service_robot turtlebot_world.launch" &
+sleep 5
+
+xterm -e "
+$source_catkin
+$export_model
+roslaunch home_service_robot view_navigation.launch;" &
+sleep 5
+
+xterm -e "
+$source_catkin
+$export_model
+roslaunch home_service_robot amcl_demo.launch;" &
+sleep 10
+
+xterm -hold -e "
+$source_catkin
+$export_model
+rosrun pick_objects pick_objects_node;" &
+sleep 5
