57260789ad0ef31b7730b57322e8d563dc229527
Kishan Patel <imkishan96@gmail.com>
2020-12-20 04:10:57 -0600

    for_video

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # change the catkin_ws path if you have different path
-source_catkin="source ~/catkin_ws/devel/setup.bash;"
+source_catkin="source ~/u_catkin_ws/devel/setup.bash;"
 
 #change the model if you wanna use another model
 export_model="export TURTLEBOT3_MODEL=burger;"
@@ -18,19 +18,19 @@ $export_model
 roslaunch home_service_robot view_navigation.launch config_file:=home_service;" &
 sleep 5
 
-xterm -e "
+xterm -T "AMCL" -geometry 70x6+0+1300 -e "
 $source_catkin
 $export_model
 roslaunch home_service_robot amcl_demo.launch;" &
 sleep 5
 
-xterm -hold -e "
+xterm -T "PICK_OBJECT" -geometry 80x9+800+1300 -e "
 $source_catkin
 $export_model
 rosrun pick_objects pick_objects_node;" &
 sleep 1
 
-xterm -hold -e "
+xterm -T "ADD_MARKER" -geometry 70x9+1700+1300 -e "
 $source_catkin
 $export_model
 rosrun add_markers add_markers_node;" &
